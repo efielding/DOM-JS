e6feede74cb2aee8aa47ad6f7f5dd78b73a33cda
Emily Waterston <emilywfielding.gmail.com>
2019-04-29 20:31:42 -0700

    conditions

---
+++ conditions.js
@@ -0,0 +1,19 @@
+// if conditions
+
+var hour = 22;
+
+// if (hour > 18){
+//     console.log('class has started');
+// }
+
+// if (hour < 21){
+//     console.log('continue class');
+// }
+
+if (hour > 18 && < 21){
+    console.log('class hours');
+} else if (hour > 24 && < 4){
+    console.log('sleep time');
+} else {
+    console.log('non class hours');
+}
