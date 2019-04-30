5a9d0321cb7d8b91cd7331ade5a69d0552813b27
Emily Waterston <emilywfielding.gmail.com>
2019-04-29 20:58:08 -0700

    started loop

---
+++ loops.js
@@ -0,0 +1,15 @@
+var fruit = ['apple', 'banana', 'peach', 'grapes', 'orange'];
+console.log(fruit);
+
+// for loop
+
+for(var i = 0; i < fruit.length; i++){
+    console.log(fruit[i]);
+}
+
+ //  do while
+var index = 7;
+do {
+    console.log(fruit[index]);
+        index++;
+ } while (index < fruit.length);
