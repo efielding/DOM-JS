bb83e2762d009e39e195a2f1ede17ca9a8d9bb92
Emily Waterston <emilywfielding.gmail.com>
2019-04-29 20:40:45 -0700

    if statement and switch

--- conditions.js
+++ conditions.js
@@ -10,10 +10,26 @@ var hour = 22;
 //     console.log('continue class');
 // }
 
-if (hour > 18 && < 21){
+if (hour > 18 && < 21) {
     console.log('class hours');
 } else if (hour > 24 && < 4){
     console.log('sleep time');
 } else {
     console.log('non class hours');
+}
+
+var minute = prompt('how many mins past the hour?');
+// switch conditions
+switch (minute){
+    case 0: 
+    console.log('hour has just begun');
+    break;
+
+    case 30:
+    console.log('half past the hour');
+    break;
+
+    default:
+    console.log('waiting for next hour');
+    break;
 }
