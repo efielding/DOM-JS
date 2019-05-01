f466024aec146ec2090d40d54ffa8dae4b85b68e
Emily Waterston <emilywfielding.gmail.com>
2019-04-30 18:47:54 -0700

    finished loops

--- loops.js
+++ loops.js
@@ -8,8 +8,32 @@ for(var i = 0; i < fruit.length; i++){
 }
 
  //  do while
-var index = 7;
-do {
-    console.log(fruit[index]);
-        index++;
- } while (index < fruit.length);
+var index = 0;
+// do {
+//     console.log(fruit[index]);
+//         index++;
+//  } while (index < fruit.length);
+
+//  while
+// while(index < fruit.length){
+//     console.log(fruit[index]);
+//     index++
+// }
+
+// while (hour > 18 && < 21){
+
+// }
+// && means both conditions must be true
+// || means first condition OR next condition = true
+
+// for in
+fruit.foo = 'test'; // foo means
+
+for (var i in fruit){
+    console.log(i);
+}
+
+//  for of
+for (var i of fruit){
+    console.log(i);
+}
